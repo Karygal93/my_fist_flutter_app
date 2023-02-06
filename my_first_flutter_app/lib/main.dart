333082fe5a43052374ff0705d0ff69a92eec23f3
Karina Galvez Abrego <Karygal93@users.noreply.github.com>
2023-02-06 11:38:34 -0600

    Update main.dart
    URL Image

--- my_first_flutter_app/lib/main.dart
+++ my_first_flutter_app/lib/main.dart
@@ -7,8 +7,16 @@ void main() {
             title: Center(child: Text('MY FIRST FUTTER APP')),
           ),
           body: const Center(
-              child: Text(
-            'This is the home page',
-            style: TextStyle(fontSize: 24),
-          )))));
+              child: Image(
+            image: NetworkImage(
+                "https://images.pexels.com/photos/5428262/pexels-photo-5428262.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"),
+          )),
+          bottomNavigationBar: TextButton(
+            child: Text(" Text Button"),
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.blueGrey,
+              primary: Colors.white,
+            ),
+            onPressed: () {},
+          ))));
 }
